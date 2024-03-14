c55586a2db4afd99d727d46e5c1c18ab45ac1d23
diego_martinez_armas <134106804+dmartinezarmas@users.noreply.github.com>
2024-03-13 23:20:45 -0300

    verifications added

--- ProductManager.js
+++ ProductManager.js
@@ -1,19 +1,30 @@
 class ProductManager {
   static #products = [];
   create(data) {
-    const product = {
-      id:
-        ProductManager.#products.length === 0
-          ? 1
-          : ProductManager.#products[ProductManager.#products.length - 1].id +
-            1,
-      title: data.title,
-      photo: data.photo,
-      category: data.category,
-      price: data.price,
-      stock: data.stock,
-    };
-    ProductManager.#products.push(product);
+    if (
+      !!data.title &
+      !!data.photo &
+      !!data.category &
+      !!data.price &
+      !!data.stock
+    ) {
+      const product = {
+        id:
+          ProductManager.#products.length === 0
+            ? 1
+            : ProductManager.#products[ProductManager.#products.length - 1].id +
+              1,
+        title: data.title,
+        photo: data.photo,
+        category: data.category,
+        price: data.price,
+        stock: data.stock,
+      };
+      ProductManager.#products.push(product);
+      console.log("Producto cargado");
+    } else {
+      console.log("Ingrese todos los datos");
+    }
   }
   read() {
     return ProductManager.#products;
@@ -33,6 +44,27 @@ productsManager.create({
   photo: "oled52.png",
   category: "tecno",
   price: "1300",
+  stock: 10,
+});
+productsManager.create({
+  title: "Tablet Samsung 10pulg",
+  photo: "tablet10.png",
+  category: "tecno",
+  price: "600",
+  stock: 12,
+});
+productsManager.create({
+  title: "Espejo smart LED",
+  photo: "espejo.png",
+  category: "fashion",
+  price: "300",
+  stock: 8,
+});
+productsManager.create({
+  title: "Sábanas premium Queen size",
+  photo: "sabanasqueen.png",
+  category: "blancos",
+  price: "300",
   stock: 10,
 });
 console.log(productsManager.read());
--- UserManager.js
+++ UserManager.js
@@ -1,32 +1,41 @@
 class UserManager {
   static #users = [];
   create(data) {
-    const user = {
-      id:
-        UserManager.#users.length === 0
-          ? 1
-          : UserManager.#users[UserManager.#users.length - 1].id + 1,
-      photo: data.photo,
-      email: data.email,
-      password: data.password,
-      role: 0,
-    };
-    UserManager.#users.push(user);
+    if (
+      !!data.photo &
+      !!data.email &
+      !!data.password
+    ) {
+      const user = {
+        id:
+          UserManager.#users.length === 0
+            ? 1
+            : UserManager.#users[UserManager.#users.length - 1].id + 1,
+        photo: data.photo,
+        email: data.email,
+        password: data.password,
+        role: 0,
+      };
+      UserManager.#users.push(user);
+      console.log("Producto cargado");
+    } else {
+      console.log("Ingrese todos los datos");
+    }
   }
-  read(){
-    return UserManager.#users
+  read() {
+    return UserManager.#users;
   }
 }
 
-const usersManager = new UserManager()
+const usersManager = new UserManager();
 usersManager.create({
-    photo: "img1.png",
-    email: "carlosromero@gmail.com",
-    password: "carlos1234"
-})
+  photo: "img1.png",
+  email: "carlosromero@gmail.com",
+  password: "carlos1234",
+});
 usersManager.create({
-    photo: "img2.png",
-    email: "pedrotorres@yahoo.com",
-    password: "pedropeter"
-})
-console.log(usersManager.read())
+  photo: "img2.png",
+  email: "pedrotorres@yahoo.com",
+  password: "pedropeter",
+});
+console.log(usersManager.read());
